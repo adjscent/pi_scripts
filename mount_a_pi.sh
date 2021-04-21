7878f14dded19eae50e413e7ac83f85ffe1f86ac
Jason <jasonsohjsh@gmail.com>
2021-04-21 17:31:30 +0800

    patch to change to loop20

--- mount_a_pi.sh
+++ mount_a_pi.sh
@@ -6,9 +6,9 @@ echo $IMG
 if [[ -e $IMG ]]; then
 	mkdir /mnt/boot
 	mkdir /mnt/rootfs
-	losetup -P /dev/loop0 "$IMG"
-	mount /dev/loop0p2 /mnt/rootfs
-	mount /dev/loop0p1 /mnt/boot
+	losetup -P /dev/loop20 "$IMG"
+	mount /dev/loop20p2 /mnt/rootfs
+	mount /dev/loop20p1 /mnt/boot
 else
   echo "Usage: $0 filename"
 fi
--- unmount_a_pi.sh
+++ unmount_a_pi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Only works with images made with Win32Imager. Does not work with ImageUSB
  
-losetup -d /dev/loop0
-umount /dev/loop0p1
-umount /dev/loop0p2
+losetup -d /dev/loop20
+umount /dev/loop20p1
+umount /dev/loop20p2
